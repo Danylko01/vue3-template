311d95db3f10fd2ce810bffe59d9607b4ff2f59f
Danylko <guanghua_dan@163.com>
2024-06-27 07:56:48 +0800

    接口封装时明确参数

--- client/src/pages/home/service.js
+++ client/src/pages/home/service.js
@@ -1,34 +1,62 @@
+/*
+ * @Description: request调用示例
+ * @Author: Danylko
+ * @Date: 2024-05-31 07:52:02
+ * @LastEditTime: 2024-06-27 07:45:05
+ */
 import request from '@/utils/request'
 
-// 发送JSON数据
-export const postData = async (data) => {
-  const response = await request.post('/login', data, {
-    headers: {
-      'Content-Type': 'application/json' // 设置 Content-Type
+// post接口示例
+export const postData = async (username, password) => {
+  try {
+    const data = {
+      username,
+      password
     }
-  })
-  return response
-}
-
-// 封装 GET 请求为同步函数
-export const getData = async (params) => {
-  const response = await request.get(
-    '/data',
-    { params },
-    {
+    const response = await request.post('/login', data, {
       headers: {
         'Content-Type': 'application/json' // 设置 Content-Type
       }
+    })
+    return response
+  } catch (error) {
+    console.error('POST 请求出错', error)
+    throw error
+  }
+}
+
+// get接口示例
+export const getData = async (username) => {
+  try {
+    const params = {
+      username
     }
-  )
-  return response
+    const response = await request.get(
+      '/data',
+      { params },
+      {
+        headers: {
+          'Content-Type': 'application/json' // 设置 Content-Type
+        }
+      }
+    )
+    return response
+  } catch (error) {
+    console.error('GET 请求出错', error)
+    throw error
+  }
 }
 
 export const getTest = async () => {
-  const response = await request.get('/test', {
-    headers: {
-      'Content-Type': 'application/json' // 设置 Content-Type
-    }
-  })
-  return response
+  try {
+    const response = await request.get('/test', {
+      headers: {
+        'Content-Type': 'application/json' // 设置 Content-Type
+      }
+    })
+    return response
+  } catch (error) {
+    console.error('GET 请求出错', error)
+    throw error
+  }
 }
